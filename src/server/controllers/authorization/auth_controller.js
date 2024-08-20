765afdbe39abcbbddbd38ca21bfcbb1e35697833
fraisai <r.uddin292@gmail.com>
2024-08-20 14:45:56 -0400

    hashed password for registration controller

--- src/server/controllers/authorization/auth_controller.js
+++ src/server/controllers/authorization/auth_controller.js
@@ -3,7 +3,9 @@ const authController = {};
 
 authController.register = async (req, res, next) => {
     try {
-        
+        const { password, email,  } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new 
     } catch (error) {
         console.log('error in authController.register'. error);
         return;
