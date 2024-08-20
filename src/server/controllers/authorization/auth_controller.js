779ea1e9996fc57b71b9f9c5f633e9df3477dbff
fraisai <r.uddin292@gmail.com>
2024-08-20 14:43:01 -0400

    adding registration endpoint

---
+++ src/server/controllers/authorization/auth_controller.js
@@ -0,0 +1,11 @@
+const authController = {};
+
+
+authController.register = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        console.log('error in authController.register'. error);
+        return;
+    }
+}
